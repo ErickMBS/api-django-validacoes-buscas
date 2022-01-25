16a565dacac92be059625b8e8e8f7b00d820ac30
erick_braga <@Erickmatheus07>
2022-01-25 11:47:10 -0300

    Configuracoes deploy

--- setup/settings.py
+++ setup/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
@@ -40,7 +41,6 @@ INSTALLED_APPS = [
 
     'rest_framework',
     'django_filters',
-
 
     'clientes'
 ]
@@ -131,3 +131,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10
 }
+
+
+# Heroku
+django_heroku.settings(locals())
