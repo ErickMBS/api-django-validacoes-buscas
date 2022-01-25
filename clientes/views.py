c2fc3c102bb451d9cf4edd63237696b6ef5ee9d7
erick_braga <@Erickmatheus07>
2022-01-25 12:12:04 -0300

    Incluir autenticacao

--- clientes/views.py
+++ clientes/views.py
@@ -2,12 +2,15 @@ from rest_framework import viewsets, filters
 from clientes.serializers import ClienteSerializer
 from clientes.models import Cliente
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 
 class ClientesViewSet(viewsets.ModelViewSet):
     """Listando clientes"""
     queryset = Cliente.objects.all()
     serializer_class = ClienteSerializer
+
     filter_backends = [
         DjangoFilterBackend,
         filters.OrderingFilter,
@@ -16,3 +19,6 @@ class ClientesViewSet(viewsets.ModelViewSet):
     ordering_fields = ['nome']
     search_fields = ['nome', 'cpf']
     filterset_fields = ['ativo']
+
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
